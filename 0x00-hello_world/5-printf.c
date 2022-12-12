7e256ea77649d629d0530a52a515efe43412d2d6
wambaria <wambaria96@gmail.com>
2022-12-12 08:46:45 -0800

    Added files 6 and 7

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-prints the phrase with proper grammer
+ * but outcome is a piece od art
+ * Return: 0 if properly exited
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h.>
+
+/**
+ * main - returns the size
+ * Returns: 0 if exited properly
+ */
+int main(void)
+{
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of int: %d byte(s)\n", sizeof(int));
+	printf("size of long int: %d byte(s)\n", sizeof(long int));
+	printf("size of long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of float: %d byte(s)\n", sizeof(float));
+	return(0);
+}

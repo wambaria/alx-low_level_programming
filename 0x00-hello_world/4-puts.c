dfb7c78403b57abbaade57ab1bb9a853c4f470d7
wambaria <wambaria96@gmail.com>
2022-12-12 06:16:13 -0800

    Added files 4 and 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * prints "Programming is like building a multilingual puzzle
+ * returns zeo id executed successfully
+ */
+int main(void)
+{
+      puts("\"Programming is like building a multilingual puzzle");
+      returns (0);
+}

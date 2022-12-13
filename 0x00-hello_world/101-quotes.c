f13cb8d63944d438371f40a770a716a5636cd3a3
wambaria <wambaria96@gmail.com>
2022-12-13 04:24:29 -0800

    Added optional tasks

---
+++ 0x00-hello_world/101-quotes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints the statement
+ * Return: Always (0)
+ */
+int main(0)
+{	
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}

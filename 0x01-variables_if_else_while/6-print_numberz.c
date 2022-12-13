5445515618f0e9416d23994af7943b279b75ac95
wambaria <wambaria96@gmail.com>
2022-12-13 10:07:01 -0800

    Added file 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the single digits numbers of base 10
+ * Return: Always 0(succes)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putcahr('\n');
+	return (0);
+}

c886a19287cc510347f278069fbed241f096b1ff
wambaria <wambaria96@gmail.com>
2022-12-13 09:21:47 -0800

    Added new files

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lowercase followed by a newline
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch = 'z'; ch++)
+		{
+			putchar(ch);
+		}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints in uppercase and then in lowercase
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch = 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n')
+	return (0)

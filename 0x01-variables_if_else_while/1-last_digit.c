8c101a6bb56240c9738426a5fcbce781da470a36
wambaria <wambaria96@gmail.com>
2022-12-13 09:48:09 -0800

    Fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 	
 	srand(time(0));
-	n = rand() - RAND_MAX / ;
+	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is %d", n, n % 10);
 	if (n % 10 > 5)
 		printf("and is greater tham 5\n");

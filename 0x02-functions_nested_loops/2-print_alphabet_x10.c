a878496f4aba3d95495341575a8b310d69e0a8e9
wambaria <wambaria96@gmail.com>
2022-12-14 10:39:32 -0800

    Added files 3,4,5

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * mainc- checks the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int round = 0;
+	char letter = 'a';
+
+	while (round < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			putchar(letter);
+			letter++:
+		}
+		_putchar('\n');
+
+		round++;
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_islower - checks for lowercase
+ *
+ * @c: c is an ascii character
+ *
+ * Return: 1 if lowercase
+ */
+
+int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}

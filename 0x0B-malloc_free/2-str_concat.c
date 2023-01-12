b98d9a24d82df95327a61aace1a0c0b1a6e1cdf3
wambaria <wambaria96@gmail.com>
2023-01-12 10:27:45 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings 
+ * str_concat - concatenates two strings
  * @s1: string one
  * @s2: string two
  *
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 ="";
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;

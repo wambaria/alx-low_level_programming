c6dd0e8d5e35da3c116c03523f2db0a06bc24118
wambaria <wambaria96@gmail.com>
2023-01-13 00:59:23 -0800

    Added five files to repo

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - malloc memory allocation
+ * @b: bytes
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}

08c7ab3d73bda0d8cd69a762e091f1edd7ce5664
wambaria <wambaria96@gmail.com>
2023-01-14 01:56:25 -0800

    Added 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - takes two numbers and multiplies them
  * @argc: number of cl arguments
  * @argv: array with command line arguments
- * Return: 0-success
+ * Return: 0 - success
  */
 
 int main(int args, char *argv[])
 {
-	if (args !=3)
+	if (args != 3)
 	{
-		print("Error\n");
+		printf("Error\n");
 		return (1);
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));

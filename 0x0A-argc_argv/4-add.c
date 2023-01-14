c5c56629b5887069f5d1a962e18736aeba38cabf
wambaria <wambaria96@gmail.com>
2023-01-14 02:18:18 -0800

    AD

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds two positive numbers
+ * @argc: number of command line arguments
+ * @argv: array with commmand line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+
+	for (i = ; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}

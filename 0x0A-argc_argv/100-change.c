f4f4128b5f4fc9ab60ef060fc9f91ed3b0533f68
wambaria <wambaria96@gmail.com>
2023-01-14 02:58:31 -0800

    Added 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -24,9 +24,9 @@ int main(int argc, char *argv[])
 		if (cents >= 25)
 			cents -= 25;
 		else if (cents >= 10)
-			cents  -= 10;
+			cents -= 10;
 		else if (cents >= 5)
-			cents -=5;
+			cents -= 5;
 		else if (cents >= 2)
 			cents -= 2;
 		else if (cents >= 1)

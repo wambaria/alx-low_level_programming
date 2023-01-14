a85170b233798cbabb1126267e086727a5ee4187
wambaria <wambaria96@gmail.com>
2023-01-14 01:51:29 -0800

    Added 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - takes two numbers and multiplies them
+ * @argc: number of cl arguments
+ * @argv: array with command line arguments
+ * Return: 0-success
+ */
+
+int main(int args, char *argv[])
+{
+	if (args !=3)
+	{
+		print("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	returb (0);
+}

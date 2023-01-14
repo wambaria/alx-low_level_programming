8786ebd351c0257d52ba58efa8ea30a722970b9e
wambaria <wambaria96@gmail.com>
2023-01-13 23:30:32 -0800

    Added 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,14 @@
-#include 
+#include <stdio.h>
+
+/**
+ * main - prints the arguments passd
+ * @argc: number of cl arguments
+ * @argv: array that contains the programs cl args
+ *
+ * Return: 0 - success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

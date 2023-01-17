54f11799f4438999968c4c3b6b04aaa18f612f1c
wambaria <wambaria96@gmail.com>
2023-01-17 09:58:45 -0800

    Fixed 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,4 @@
-#ifndef function_like_macro_h
-#define function_like_macro_h
-
-#define ABS(x) ((x < 0) ? (x) * (-1) : (x));
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACROH
+#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
 #endif

d67d1d2aac22babcc2454d6d8f190d15e49a8994
wambaria <wambaria96@gmail.com>
2023-01-24 01:13:29 -0800

    Added tasks

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * @separator: string to be separated
+ * @n: no of integers passed to the functon
+ * Return: No return
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	va_start(valist, n);
+
+	for(i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(va_list, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+
+	print("%n");
+	va_end(valist);
+}

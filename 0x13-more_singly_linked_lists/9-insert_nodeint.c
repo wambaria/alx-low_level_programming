8ed744619396ba0b0ab890089671ff168e57789f
wambaria <wambaria96@gmail.com>
2023-02-22 10:01:58 -0800

    Fixed 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,7 +5,7 @@
  * @head: head of a list.
  * @idx: index of the list where the new node is added.
  * @n: integer element.
- * Return: the address of the new node, or NULL 
+ * Return: the address of the new node, or NULL.
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)

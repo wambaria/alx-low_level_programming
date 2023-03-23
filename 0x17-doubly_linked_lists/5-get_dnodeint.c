e1481dcae59ea070f23360fdb1ae1f8d1a2f3319
wambaria <wambaria96@gmail.com>
2023-03-23 10:41:55 -0700

    Added 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,7 @@
  * get_dnodeint_at_index - returns the
  * nth node of a dlistint_t
  * @head: head
- * @index: index 
+ * @index: index
  * Return: nth node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)

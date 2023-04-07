1e9e7164733677090da8cf817c8ded607873d25b
wambaria <wambaria96@gmail.com>
2023-04-07 10:50:27 -0700

    Added 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,71 +1,72 @@
-#include "hash_table.h"
+#include "hash_tables.h"
+
+/**
+ * hash_node - creates a new hash node
+ * @key: key for the node
+ * @value: for the node
+ * Return: the new node, or NULL on failure
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
 
 
 /**
- * make_hash_node - creates a new hash node
- * @key: key for the node
- * @value: for the node
- *
- * Return: the new node, or NULL on failure
+ * hash_table_set - sets a key to a value in the hash table
+ * @ht: hash table to add element
+ * @key: key
+ * @value: data
+ * Return: 1 if successful, 0 if not
  */
-
-unsigned long hash(const char *str)
-{
-    unsigned long hash = 5381;
-    int c;
-
-    while ((c = *str++))
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
-    return hash;
-}
-
-/* Creates a new node with the given key and value */
-hash_node_t *create_node(const char *key, const char *value)
-{
-    hash_node_t *node = malloc(sizeof(hash_node_t));
-    if (node == NULL) {
-        return NULL;
-    }
-
-    node->key = strdup(key);
-    node->value = strdup(value);
-    node->next = NULL;
-
-    return node;
-}
-
-/* Adds a new node to the hash table with the given key and value */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    if (key == NULL || strlen(key) == 0) {
-        return 0;
-    }
+	unsigned long int index;
+	hash_node_t *hash_node, *tmp;
+	char *new_value;
 
-    unsigned long hash_value = hash(key) % ht->size;
-    hash_node_t *node = ht->array[hash_value];
-
-    /* Check if key already exists in the hash table */
-    while (node != NULL) {
-        if (strcmp(node->key, key) == 0) {
-            free(node->value);
-            node->value = strdup(value);
-            return 1;
-        }
-        node = node->next;
-    }
-
-    /* Key does not exist, create a new node */
-    hash_node_t *new_node = create_node(key, value);
-    if (new_node == NULL) {
-        return 0;
-    }
-
-    /* Add the new node at the beginning of the list */
-    new_node->next = ht->array[hash_value];
-    ht->array[hash_value] = new_node;
-    ht->count++;
-
-    return 1;
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			free(tmp->value);
+			tmp->value = new_value;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	hash_node = make_hash_node(key, value);
+	if (hash_node == NULL)
+		return (0);
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
+	return (1);
 }
 
